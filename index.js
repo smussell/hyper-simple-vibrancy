df2528eede9814375bad0232fae757c3924badfd
Sam Mussell <smussell@gmail.com>
2016-12-13 06:47:42 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports.onWindow = browserWindow => browserWindow.setVibrancy('dark');
 
 module.exports.decorateConfig = config => {
-  config.backgroundColor = 'rgba(0,0,0,.5)';
+  config.backgroundColor = 'rgba(0,0,0,.8)';
   return config;
 }
